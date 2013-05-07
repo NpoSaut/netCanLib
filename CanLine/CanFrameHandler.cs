007a0480a4009f3fd1e47696a249588150e2aadb
Evgeniy Plyusnin <eninbox@gmail.com>
2013-05-07 15:17:39 +0600

    Выпилена возможность отслеживания состояния переменных

--- CanLine/CanFrameHandler.cs
+++ CanLine/CanFrameHandler.cs
@@ -17,7 +17,6 @@ namespace Communications.Can
         public CanFrameHandler(int Descriptor)
         {
             this.Descriptor = Descriptor;
-            this.ValueValidnessTime = TimeSpan.FromMilliseconds(1500);
             this.OnPorts = new List<CanPort>();
 
             lock (AllHandlers)
@@ -26,8 +25,6 @@ namespace Communications.Can
 
         internal void OnRecieved(IList<CanFrame> Frames, CanPort FromPort)
         {
-            SetLastFrame(Frames.Last());
-
             if (Recieved != null) Recieved(this, new CanFramesReceiveEventArgs(Frames, FromPort));
 
             lock (WaitLocker)
@@ -43,56 +40,6 @@ namespace Communications.Can
         private Boolean IsWaiting { get; set; }
         private object WaitLocker = new object();
         private CanFrame PendingFrame;
-
-        private object LastValueLocker = new object();
-        public TimeSpan ValueValidnessTime { get; set; }
-        public CanFrame LastFrame { get; private set; }
-        public DateTime LastFrameRecieveTime { get; private set; }
-        private bool _LastFrameValid;
-        public event EventHandler LastFrameValidChanged;
-        /// <summary>
-        /// Показывает, актуально ли значение последнего принятого кадра
-        /// </summary>
-        public bool LastFrameValid
-        {
-            get { return _LastFrameValid; }
-            private set
-            {
-                if (_LastFrameValid != value)
-                {
-                    _LastFrameValid = value;
-                    if (LastFrameValidChanged != null) LastFrameValidChanged(this, new EventArgs());
-                }
-            }
-        }
-        private void SetLastFrame(CanFrame Frame)
-        {
-            lock (LastValueLocker)
-            {
-                LastFrame = Frame;
-                LastFrameRecieveTime = DateTime.Now;
-                LastFrameValid = true;
-            }
-        }
-
-        private static void LastFrameValidnessCheckJob()
-        {
-            while (true)
-            {
-                lock (AllHandlers)
-                {
-                    foreach (var h in AllHandlers)
-                    {
-                        DateTime dt = DateTime.Now;
-                        lock (h.LastValueLocker)
-                        {
-                            h.LastFrameValid = h.LastFrameValid && (h.LastFrameRecieveTime.Add(h.ValueValidnessTime) >= dt);
-                        }
-                    }
-                }
-                Thread.Yield();
-            }
-        }
 
         public CanFrame WaitFor()
         {
@@ -110,9 +57,6 @@ namespace Communications.Can
         static CanFrameHandler()
         {
             AllHandlers = new List<CanFrameHandler>();
-            Thread ValidnessCheckThread = new Thread(new ThreadStart(LastFrameValidnessCheckJob));
-            ValidnessCheckThread.IsBackground = true;
-            ValidnessCheckThread.Start();
         }
 
         public void Dispose()
