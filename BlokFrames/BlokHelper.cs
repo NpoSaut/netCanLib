0a81ebd6519d004dc1d4b5fa9c98fc8645bbfb97
Evgeniy Plyusnin <eninbox@gmail.com>
2013-06-25 17:06:50 +0400

    Добавлены различные практические вещи:  + Методы, извлекающие дескриптор для типа фрейма сделаны публичными  + Добавлен метод-расширение, который выделяет сообщения БЛОК-а из потока CAN-сообщений  + ещё что-то, но в этом УАЗике так душно, пыльно и жарко, что я уже не могу вспомнить ничего :(

---
+++ BlokFrames/BlokHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Communications.Can;
+
+namespace BlokFrames
+{
+    public static class BlokHelper
+    {
+        /// <summary>
+        /// Выбирает из последовательности CAN-сообщений в БЛОК-сообщения указанного типа
+        /// </summary>
+        /// <typeparam name="T">Тип сообщений БЛОК</typeparam>
+        /// <param name="FramesFlow">Поток CAN-сообдений</param>
+        /// <returns>Последовательность сообщений БЛОК указанного типа</returns>
+        public static IEnumerable<T> OfFrameType<T>(this IEnumerable<CanFrame> FramesFlow)
+            where T: BlokFrame, new()
+        {
+            var FilteringDescriptors = new HashSet<int>();
+            FilteringDescriptors.Add(BlokFrame.GetDescriptor<T>());
+            return FramesFlow.Where(f => FilteringDescriptors.Contains(f.Descriptor)).Select(f => BlokFrame.GetBlokFrame<T>(f));
+        }
+    }
+}
