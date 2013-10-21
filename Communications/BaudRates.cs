3ca097f27f19ed0dd2329334a2af58e96d5b8da2
Evgeniy Plyusnin <eninbox@gmail.com>
2013-10-21 16:19:30 +0600

    Исправил позорную опечатку (бод/сек)

--- Communications/BaudRates.cs
+++ Communications/BaudRates.cs
@@ -7,19 +7,19 @@ namespace Communications
 {
     public static class BaudRates
     {
-        /// <summary>100 Кбод/сек</summary>
+        /// <summary>100 Кбод</summary>
         public const int CBR_100K = 100000;
 
-        /// <summary>50 Кбод/сек</summary>
+        /// <summary>50 Кбод</summary>
         public const int CBR_50K = 50000;
 
-        /// <summary>25 Кбод/сек</summary>
+        /// <summary>25 Кбод</summary>
         public const int CBR_25K = 25000;
 
-        /// <summary>9600 бод/сек</summary>
+        /// <summary>9600 бод</summary>
         public const int CBR_9600 = 9600;
 
-        /// <summary>115200 бод/сек</summary>
+        /// <summary>115200 бод</summary>
         public const int CBR_115200 = 115200;
     }
 }
