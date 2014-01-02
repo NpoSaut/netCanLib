a3bd9a36afc0ca15fddbfad2c8297cbb35bf8640
Evgeniy Plyusnin <eninbox@gmail.com>
2014-01-02 15:13:36 +0600

    Первые наброски машины состояний

---
+++ IsoTP/States/IsoTpState.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using Communications.Protocols.IsoTP.Frames;
+
+namespace Communications.Protocols.IsoTP.States
+{
+    abstract class IsoTpState
+    {
+        public ITransactionContext Context { get; private set; }
+        public IsoTpState(ITransactionContext Context) { this.Context = Context; }
+
+        public abstract void ProcessFrame(IsoTpFrame Frame);
+        
+        public event EventHandler<ChangeStateRequestEventArgs> StateChangetRequested;
+        protected virtual void OnStateChangetRequested(ChangeStateRequestEventArgs E)
+        {
+            var handler = StateChangetRequested;
+            if (handler != null) handler(this, E);
+        }
+
+        public event EventHandler<ExceptionHandledEventArgs> ExceptionHandeled;
+        protected virtual void OnExceptionHandeled(HandledEventArgs E)
+        {
+            var handler = ExceptionHandeled;
+            if (handler != null) handler(this, E);
+        }
+    }
+
+    class ChangeStateRequestEventArgs : EventArgs
+    {
+        public IsoTpState NextState { get; private set; }
+        public ChangeStateRequestEventArgs(IsoTpState NextState) { this.NextState = NextState; }
+    }
+
+    class ExceptionHandledEventArgs : EventArgs
+    {
+        public Exception HandeledException { get; private set; }
+        public ExceptionHandledEventArgs(Exception HandeledException) { this.HandeledException = HandeledException; }
+    }
+
+    /// <summary>
+    /// Состояние отправки последовательных фреймов
+    /// </summary>
+    class ConsecutiveReceiveState : IsoTpState
+    {
+        public ConsecutiveReceiveState(ITransactionContext Context) : base(Context) { }
+
+        private int BlocksCounter { get; set; }
+
+        public override void ProcessFrame(IsoTpFrame Frame)
+        {
+            var cFrame = Frame as ConsecutiveFrame;
+            if (cFrame == null) throw new Exception();
+            Context.DataStream.Write(cFrame.Data, 0, cFrame.Data.Length);
+            BlocksCounter++;
+            if (BlocksCounter >= Context.BlockSize) SendControlFrame();
+        }
+          
+        private void SendControlFrame()
+        {
+            var flowControlFrame = new FlowControlFrame(FlowControlFlag.ClearToSend,
+                                                        (byte)Context.BlockSize,
+                                                        Context.SeparationTime);
+            Context.CanFlow.Send(flowControlFrame.GetCanFrame(Context.SenderDescriptor));
+        }
+    }
+}
---
+++ IsoTP/TransactionContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Communications.Can;
+
+namespace Communications.Protocols.IsoTP
+{
+    internal interface ITransactionContext
+    {
+        /// <summary>Задержка перед отправкой очередного фрейма</summary>
+        TimeSpan SeparationTime { get; }
+        /// <summary>Размер одного блока</summary>
+        int BlockSize { get; }
+        /// <summary>Поток данных (входящих или исходящих)</summary>
+        MemoryStream DataStream { get; }
+        /// <summary>Поток Can-сообщений</summary>
+        ICanFlow CanFlow { get; }
+        /// <summary>Дескриптор отправляющей стороны</summary>
+        int SenderDescriptor { get; }
+        /// <summary>Дескриптор принимающей стороны</summary>
+        int ReceiverDescriptor { get; }
+    }
+
+    class MemoryTransactionContext : ITransactionContext
+    {
+        public TimeSpan SeparationTime { get; private set; }
+        public int BlockSize { get; private set; }
+        public MemoryStream DataStream { get; private set; }
+        public ICanFlow CanFlow { get; private set; }
+        public int SenderDescriptor { get; private set; }
+        public int ReceiverDescriptor { get; private set; }
+
+        public MemoryTransactionContext(TimeSpan SeparationTime, int BlockSize, int DataLength, ICanFlow CanFlow,
+                                  int SenderDescriptor, int ReceiverDescriptor)
+        {
+            this.ReceiverDescriptor = ReceiverDescriptor;
+            this.SenderDescriptor = SenderDescriptor;
+            this.CanFlow = CanFlow;
+            this.BlockSize = BlockSize;
+            this.SeparationTime = SeparationTime;
+            DataStream = new MemoryStream(DataLength);
+        }
+    }
+}
