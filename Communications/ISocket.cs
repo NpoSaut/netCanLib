e37265b8ec0c2fbb788a95be236bac9e8dbd2f4a
Evgeniy Plyusnin <eninbox@gmail.com>
2014-08-06 14:30:17 +0600

    Отформатирован ISocket

--- Communications/ISocket.cs
+++ Communications/ISocket.cs
@@ -4,37 +4,38 @@ using Communications.Exceptions;
 
 namespace Communications
 {
-    /// <summary>
-    /// Интерфейс сокета
-    /// </summary>
-    public interface ISocket : INamable, IClosable
-    { }
+    /// <summary>Интерфейс сокета</summary>
+    public interface ISocket : INamable, IClosable { }
 
-    /// <summary>
-    /// Интерфейс сокета
-    /// </summary>
+    /// <summary>Интерфейс сокета</summary>
     /// <typeparam name="TDatagram">Тип дейтаграммы сокета</typeparam>
     public interface ISocket<TDatagram> : ISocket
     {
         /// <summary>Отправляет дейтаграмму в сокет</summary>
         void Send(TDatagram Data);
+
         /// <summary>Отправляет дейтаграммы в сокет</summary>
         void Send(IEnumerable<TDatagram> Data);
 
-        /// <summary>
-        /// Производит блокирующее считывание данных из сокета
-        /// </summary>
+        /// <summary>Производит блокирующее считывание данных из сокета</summary>
         /// <param name="Timeout">Таймаут на операцию считывания</param>
-        /// <param name="ThrowExceptionOnTimeout">Указывает, следует ли вызывать исключение при превышении таймаута</param>
+        /// <param name="ThrowExceptionOnTimeout">
+        ///     Если true, то при превышении времени ожидания очередного сообщения будет
+        ///     выбрасываться <see cref="SocketReadTimeoutException" />, если нет -- просто будет перечисление коллекции будет
+        ///     прекращено
+        /// </param>
         /// <returns>Последовательность считанных дейтаграмм</returns>
         /// <remarks>
-        /// Поскольку функция блокирующая и считывает _все_ приходящие кадры, выход из этого перечисления не предусмотрен.
-        /// Для правильной работы следует пользоваться лишь методами вроде методов, извлекающих N первых членов последовательности (First, Take, TakeWhile, ...)
-        /// 
-        /// Можно использовать для отлова серий подряд идущих сообщений (когда другая серия отделяется по времени).
-        /// Параметр ThrowExceptionOnTimeout следует использовать, если сообщение гарантированно должно поступить и его задержка является признаком ошибки
+        ///     Поскольку функция блокирующая и считывает _все_ приходящие кадры, выход из этого перечисления не предусмотрен.
+        ///     Для правильной работы следует пользоваться лишь методами вроде методов, извлекающих N первых членов
+        ///     последовательности (First, Take, TakeWhile, ...) Можно использовать для отлова серий подряд идущих сообщений (когда
+        ///     другая серия отделяется по времени). Параметр ThrowExceptionOnTimeout следует использовать, если сообщение
+        ///     гарантированно должно поступить и его задержка является признаком ошибки
         /// </remarks>
-        /// <exception cref="SocketReadTimeoutException">При превышении времени ожидания сообщения (если <paramref name="ThrowExceptionOnTimeout"/> == true)</exception>
+        /// <exception cref="SocketReadTimeoutException">
+        ///     При превышении времени ожидания сообщения (если
+        ///     <paramref name="ThrowExceptionOnTimeout" /> == true)
+        /// </exception>
         IEnumerable<TDatagram> Receive(TimeSpan Timeout = default(TimeSpan), bool ThrowExceptionOnTimeout = false);
     }
 }
