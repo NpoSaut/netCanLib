fa0b9b6519e3cfe92f394f147b8e82be1b28fa23
Evgeniy Plyusnin <eninbox@gmail.com>
2014-08-06 15:52:21 +0600

    Добавление в проект файлов в Communications/Piling/...

---
+++ Communications/Piping/PackageFeedbackSendingPipeBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using Communications.Piping.Packages;
+
+namespace Communications.Piping
+{
+    public abstract class PackageFeedbackSendingPipeBase<TDatagram> : ISendPipe<TDatagram>
+    {
+        private readonly object _sendLocker = new object();
+
+        /// <summary>Передаёт дейтаграммы на низлежащий уровень для его отправки</summary>
+        public void Send(IList<TDatagram> Frames)
+        {
+            IOutgoingPack<TDatagram> package = Enpack(Frames);
+            lock (_sendLocker)
+            {
+                PushToSend(Frames);
+            }
+            SpinWait.SpinUntil(() => package.IsDone);
+        }
+
+        protected abstract IOutgoingPack<TDatagram> Enpack(IList<TDatagram> Datagrams);
+        protected abstract void PushToSend(IList<TDatagram> Datagrams);
+    }
+}
