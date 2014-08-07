dffe23458e186af7e907cfec2e5fdfbe69ab7114
Evgeniy Plyusnin <eninbox@gmail.com>
2014-08-07 11:51:05 +0600

    Исправил ошибку в DirectSocketCanReader: раньше он просто переправлял чтение входящих сообщений, как есть. Теперь - выдерживает таймаует чтения, циклически вызывая функцию выгребания сообщений.

--- SocketCanWorking/ReadersWriters/DirectSocketCanReader.cs
+++ SocketCanWorking/ReadersWriters/DirectSocketCanReader.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using Communications.Can;
+using Communications.Exceptions;
 using SocketCanWorking.LinuxSockets;
 
 namespace SocketCanWorking.ReadersWriters
@@ -12,6 +15,21 @@ namespace SocketCanWorking.ReadersWriters
 
         /// <summary>Выполняет блокирующее чтение из сокета</summary>
         /// <param name="Timeout">Таймаут чтения</param>
-        public IEnumerable<CanFrame> Receive(TimeSpan Timeout) { return _linuxSocket.Receive(Timeout); }
+        public IEnumerable<CanFrame> Receive(TimeSpan Timeout)
+        {
+            // Засекаем время
+            var sw = new Stopwatch();
+            sw.Start();
+
+            // Продолжаем загребать из буфера, пока не выйдет таймаут
+            while (sw.Elapsed <= Timeout)
+            {
+                IList<CanFrame> frames = _linuxSocket.Receive(Timeout - sw.Elapsed);
+                foreach (CanFrame frame in frames) yield return frame;
+                // Если получено хоть одно сообщение, сбрасываем таймер
+                if (frames.Any()) sw.Restart();
+            }
+            throw new SocketReadTimeoutException();
+        }
     }
 }
