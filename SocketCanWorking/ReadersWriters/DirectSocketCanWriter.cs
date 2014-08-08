ffc248f1ca580c7fe7e086c30714180fb3917a2e
Evgeniy Plyusnin <eninbox@gmail.com>
2014-08-08 14:47:44 +0600

    Функционал по нарезке пачки сообщений на отправку выделен из DirectSocketCanWriter в SlicetSocketCanWriterBase и реализован через возвращаемое значение функции ILunixSocket.Send()

--- SocketCanWorking/ReadersWriters/DirectSocketCanWriter.cs
+++ SocketCanWorking/ReadersWriters/DirectSocketCanWriter.cs
@@ -1,30 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Communications.Can;
 using SocketCanWorking.LinuxSockets;
 
 namespace SocketCanWorking.ReadersWriters
 {
-    public class DirectSocketCanWriter : ISocketCanWriter
+    public class DirectSocketCanWriter : SlicedSocketCanWriterBase
     {
         private readonly ILinuxSocket _linuxSocket;
         public DirectSocketCanWriter(ILinuxSocket LinuxSocket) { _linuxSocket = LinuxSocket; }
 
-        /// <summary>Выполняет блокирующую отправку сообщений в линию</summary>
-        public void Send(IList<CanFrame> Frames)
-        {
-            if (Frames.Count < _linuxSocket.TxBufferSize) _linuxSocket.Send(Frames);
-            else
-            {
-                int pointer = 0;
-                while (pointer < Frames.Count)
-                {
-                    int count = Math.Min(_linuxSocket.TxBufferSize, Frames.Count - pointer);
-                    _linuxSocket.Send(Frames.Skip(pointer).Take(count).ToList());
-                    pointer += count;
-                }
-            }
-        }
+        /// <summary>Ставит сообщения на отправку</summary>
+        /// <param name="Frames">Список сообщений на отправку</param>
+        /// <returns>Количество сообщений из списка, поставленных на отправку</returns>
+        protected override int Push(IList<CanFrame> Frames) { return _linuxSocket.Send(Frames); }
     }
 }
