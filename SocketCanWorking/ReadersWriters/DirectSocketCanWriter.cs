d00f9b2be1ca17b29935c1f2b545bfd50e6cfd7d
Evgeniy Plyusnin <eninbox@gmail.com>
2014-08-08 14:37:02 +0600

    DirectSocketCanWriter теперь учитывает размер буфера исходящих сообщений в сокете и не превышает его

--- SocketCanWorking/ReadersWriters/DirectSocketCanWriter.cs
+++ SocketCanWorking/ReadersWriters/DirectSocketCanWriter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Communications.Can;
 using SocketCanWorking.LinuxSockets;
 
@@ -10,6 +12,19 @@ namespace SocketCanWorking.ReadersWriters
         public DirectSocketCanWriter(ILinuxSocket LinuxSocket) { _linuxSocket = LinuxSocket; }
 
         /// <summary>Выполняет блокирующую отправку сообщений в линию</summary>
-        public void Send(IList<CanFrame> Frames) { _linuxSocket.Send(Frames); }
+        public void Send(IList<CanFrame> Frames)
+        {
+            if (Frames.Count < _linuxSocket.TxBufferSize) _linuxSocket.Send(Frames);
+            else
+            {
+                int pointer = 0;
+                while (pointer < Frames.Count)
+                {
+                    int count = Math.Min(_linuxSocket.TxBufferSize, Frames.Count - pointer);
+                    _linuxSocket.Send(Frames.Skip(pointer).Take(count).ToList());
+                    pointer += count;
+                }
+            }
+        }
     }
 }
