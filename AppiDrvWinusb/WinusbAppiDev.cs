1583dc08dcb2f83c9178a08c12d6d7a49eb04dc7
Evgeniy Plyusnin <eninbox@gmail.com>
2016-03-23 16:44:26 +0500

    Убрал из списка поддерживаемых устройств GUID для АППИ-Блок

--- AppiDrvWinusb/WinusbAppiDev.cs
+++ AppiDrvWinusb/WinusbAppiDev.cs
@@ -18,7 +18,6 @@ namespace Communications.Appi.Winusb
         public static readonly List<string> DeviceGuids =
             new List<string>
             {
-                "524cc09a-0a72-4d06-980e-afee3131196e",
                 "3af3f480-41b5-4c24-b2a9-6aacf7de3d01"
             };
 
